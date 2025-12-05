40e804718c13d57627448a555bc32d5208f5e92e
Naveez Munir <naveezmunir@gmail.com>
2025-12-05 14:30:00 +0500

    feat: add utility functions

---
+++ lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * Utility function to merge Tailwind CSS classes safely
+ * Handles conflicts and deduplication using clsx and tailwind-merge
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
